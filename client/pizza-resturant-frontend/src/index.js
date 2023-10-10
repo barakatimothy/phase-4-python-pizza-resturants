b8c93e090a898dc48011bb342e74a57fac3dfd7e
barakatimothy <timothybaraka39@gmail.com>
2023-10-10 19:45:37 +0300

    Chore: Removed all unecessary files

--- client/pizza-resturant-frontend/src/index.js
+++ client/pizza-resturant-frontend/src/index.js
@@ -1,23 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Pizzas from './components/Pizzas';
-import RestaurantDetails from './components/RestaurantDetails';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/pizzas" component={Pizzas} />
-      <Route path="/restaurant/:id" component={RestaurantDetails} />
-    </Switch>
-  </Router>,
-  document.getElementById('root')
-);
-
 
 
